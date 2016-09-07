a13d96e0f98a6c398bc11e588aaa192eb37708a4
Александр <vojtseh2014@gmail.com>
2016-09-07 22:04:35 +0300

    Update urls.py

--- Primes/urls.py
+++ Primes/urls.py
@@ -3,7 +3,5 @@ from django.contrib import admin
 from Primes import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'^get_prime/(?P<value>[0-9]+)/', views.browser),
-    url(r'^$', views.index),
 ]
